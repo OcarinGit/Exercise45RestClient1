bf683afe8c4d24580b2378545e5c55c28b34829a
OcarinGit <oscarsantanaalvarez@gmail.com>
2019-12-12 14:13:44 -0600

    Rest client for GET requests

---
+++ src/com/exercise45restclient/app/Exercise45RestClientApp.java
@@ -0,0 +1,18 @@
+package com.exercise45restclient.app;
+
+import javax.json.JsonArray;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+public class Exercise45RestClientApp {
+
+	public static void main(String[] args) {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080/Exercise45Rest/Customer");
+		JsonArray response = target.request(MediaType.APPLICATION_JSON).get(JsonArray.class);
+		System.out.println(response);
+	}
+
+}
